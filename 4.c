6d3f7d89adf5cd7c7c7c15c36f1430dc24246915
PJJ <1974696266@qq.com>
2025-02-26 20:37:47 +0800

    删除所有 Git 跟踪的文件

--- 4.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	printf("*********************************\n");
-	printf("*         Hello，World!         *\n");
-	printf("*********************************\n");
-}
